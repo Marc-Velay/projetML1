ee30b01eba3673b4a6b9b52880c5a0ced07d0559
Marc-Velay <marc.velay@gmail.com>
2018-10-19 09:44:30 +0200

    modified reading script

--- data_analysis.py
+++ data_analysis.py
@@ -1,9 +1,25 @@
 import pandas as pd
-data = pd.read_csv(“adult.data.txt",
-                    names=["Age", "Workclass", "fnlwgt", "Education", "Education-Num", "Martial
-                    Status", “Occupation", "Relationship", "Race", "Sex", "Capital Gain", "Capital
-                    Loss", “Hours per week", "Country", "Target"],
+from collections import Counter
+
+data = pd.read_csv("data/adult.data",
+                    names=["Age", "Workclass", "fnlwgt", "Education", "Education-Num", "Martial Status",
+                    "Occupation", "Relationship", "Race", "Sex", "Capital Gain", "Capital Loss",
+                    "Hours per week", "Country", "Target"],
                     sep=r'\s*,\s*',
                     engine='python',
                     na_values="?")
-data.tail()
+#pprint(data)
+
+#print(data["Workclass"])
+
+#print(data.isnull())
+
+print(Counter(list(data["Workclass"])))
+print(Counter(list(data["Workclass"])))
+print(Counter(list(data["Workclass"])))
+
+'''
+for index, row in data.iterrows():
+    print(row)
+    input()
+'''
